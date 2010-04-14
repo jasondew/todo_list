4d24aa4822161553333c46d2d558abab09c2092f
Jason Dew <jason.dew@gmail.com>
2010-04-14 16:10:36 -0400

    using sass_on_heroku instead

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,6 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "hassle"
   config.gem "haml", :source => "http://gemcutter.org/"
   config.gem "shoulda"
   config.gem "factory_girl"
